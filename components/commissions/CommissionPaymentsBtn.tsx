75987b718672939cd8333ed2b7cbc5759b47f6ed
Byaxy <charlesbyaxy@gmail.com>
2025-11-28 13:45:55 +0000

    commission payments btn link

--- components/commissions/CommissionPaymentsBtn.tsx
+++ components/commissions/CommissionPaymentsBtn.tsx
@@ -1,18 +1,14 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { Button } from "../ui/button";
+import Link from "next/link";
 
 const CommissionPaymentsBtn = () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push("/accounting-and-finance/commissions/payments");
-  };
   return (
-    <Button className="shad-primary-btn" type="button" onClick={handleClick}>
+    <Link
+      href={"/accounting-and-finance/commissions/payments"}
+      prefetch={true}
+      className="shad-primary-btn flex flex-row items-center justify-center gap-1 px-4 py-2 rounded-md shadow-sm text-sm"
+    >
       Commission Payments
-    </Button>
+    </Link>
   );
 };
 
